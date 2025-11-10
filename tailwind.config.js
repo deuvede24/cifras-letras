e61c09dd7f394d27d0911755fbbf64b3459f8a11
deuvede24 <cdaniela.vd@gmail.com>
2025-11-10 14:07:38 +0100

    chore(config): Angular, Tailwind and global styles setup

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#3B82F6',
+        secondary: '#8B5CF6',
+        success: '#10B981',
+        error: '#EF4444',
+        accent: '#FBBF24',
+      }
+    },
+  },
+  plugins: [],
+}
