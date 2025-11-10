78ae3a242bbb8cd908f708595b73a00a4429f85b
deuvede24 <cdaniela.vd@gmail.com>
2025-11-10 14:05:40 +0100

    feat(core): base app component and routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./components/menu/menu.component').then(m => m.MenuComponent)
+  },
+  {
+    path: 'letras',
+    loadComponent: () => import('./components/letras/letras.component').then(m => m.LetrasComponent)
+  },
+  {
+    path: 'cifras',
+    loadComponent: () => import('./components/cifras/cifras.component').then(m => m.CifrasComponent)
+  }
+];
